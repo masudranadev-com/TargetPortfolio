f3d6b0c78b9b93d585255d7f6791086b9b804b48
MR PC <masudranamd112@gmail.com>
2026-07-18 01:49:57 +0600

    Limit Next build workers for shared hosting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    cpus: 1,
+    staticGenerationMaxConcurrency: 1,
+    staticGenerationMinPagesPerWorker: 1000,
+  },
 };
 
 export default nextConfig;
